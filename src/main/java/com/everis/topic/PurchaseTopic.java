472a20bf2fd0f0a3ca7ccc433ee3740e7abcb785
Alejandro <alejo_540@hotmail.com>
2021-07-07 17:26:43 -0500

    First Commit

---
+++ src/main/java/com/everis/topic/PurchaseTopic.java
@@ -0,0 +1,60 @@
+package com.everis.topic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+@Configuration
+public class PurchaseTopic {
+  
+  @Value("${kafka.server.hostname}")
+  private String hostName;
+  
+  @Value("${kafka.server.port}")
+  private String port;
+  
+  @Bean
+  public NewTopic createPurchaseTopic() {
+  
+    return TopicBuilder
+      .name("created-purchase-topic")
+      .partitions(1)
+      .replicas(1)
+      .build();
+  
+  }
+
+  @Bean
+  public ProducerFactory<String, Object> producerFactory() {
+  
+  Map<String, Object> config = new HashMap<>();
+  
+  config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, hostName + ":" + port);
+  
+  config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+  
+  config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+  
+  return new DefaultKafkaProducerFactory<>(config);
+  
+  }
+  
+  @Bean
+  public KafkaTemplate<String, Object> kafkaTemplate() {
+  
+  return new KafkaTemplate<>(producerFactory());
+  
+  }
+  
+}
